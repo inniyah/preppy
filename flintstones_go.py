e5c6268962d8130b45e793fd0aa315109380d882
aaron <aaron@localhost>
2000-10-09 14:32:41 +0000

    added optional file output argument and option to not generate *.py file, updated examples and documentation

--- flintstones_go.py
+++ flintstones_go.py
@@ -10,4 +10,20 @@ from preppy import getModule
 result = getModule(name, verbose=1)
 print "="*77
 D =  {'sex': 'm', 'name': 'george'}
-result.run(D)
+import sys
+# don't do this
+#result.run(D, __write__=sys.stdout.write, outputfile=open("flintstone.html", "w"))
+
+# do this
+hname = name+".html"
+print "generating", hname
+result.run(D, outputfile=open(hname, "w"))
+
+
+print "="*77
+# or this
+print "now generating to stdout"
+result.run(D, __write__=sys.stdout.write)
+
+# or this (default to __write__ = sys.stdout.write)
+#result.run(D)
