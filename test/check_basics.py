e2b0be253236183a3de86ec69a58709e368340c6
john <john@reportlab.com>
2000-11-13 12:05:50 +0000

    Added a new test case, and two extras added which don't behave as expected (look at 'check11_StringKeysThatLookLikeIntegers' and 'check12_StringKeysThatLookLikeFloats').

--- test/check_basics.py
+++ test/check_basics.py
@@ -1,30 +1,35 @@
 #preppy test
-import os, glob, string
+import os, glob, string, random
 from rlextra.preppy import preppy
 from reportlab.test import unittest
 
 
 class SimpleTestCase(unittest.TestCase):
-    def check1ScriptTag(self):
+    def check01ScriptTag(self):
         processTest('sample001')
-    def check2ForAndWhileLoops(self):
+
+    def check02ForAndWhileLoops(self):
         processTest('sample002')
-    def check3IfTags(self):
+
+    def check03IfTags(self):
         processTest('sample003')
-    def check4PassingDictionaries(self):
+
+    def check04PassingDictionaries(self):
         dictionary = {"song1": {"songtitle": "The Lumberjack Song", "by": "Monthy Python"},
                       "song2": {"songtitle": "My Way", "by": "Frank Sinatra"}}
         processTest('sample004', dictionary)
-    def check5Escapes(self):
+
+    def check05Escapes(self):
         processTest('sample005')
-    def check6NoPythonFile(self):
+
+    def check06NoPythonFile(self):
         mod = preppy.getModule('sample006', savefile=0)
         outFile = open('sample006.html', 'w')
         mod.run(dictionary={}, outputfile = outFile)
         outFile.close()
         print 'wrote sample006.html'
 
-    def check7NoOutputFile(self):
+    def check07NoOutputFile(self):
         sourceString = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
         <HTML>
         <HEAD>
@@ -53,8 +58,10 @@ class SimpleTestCase(unittest.TestCase):
         <P>
         
         Below is a listing of the files in the same directory as this HTML file. There should be no file called <I>sample007.prep</I>.
-        
-        <BR><BR><CENTER>
+
+                
+        <BR><HR>
+        <BR><CENTER>
         <TABLE>
         {{script}}
         import os
@@ -70,38 +77,34 @@ class SimpleTestCase(unittest.TestCase):
         for item in range(1,len(fileList)-1,2):
             if os.path.isfile(fileList[item]):
                 rightColumn.append(fileList[item])
-        
+
         for loopCounter in range(0, len(leftColumn)):
-            print "<TR><TD WIDTH=30%>"
-            print leftColumn[loopCounter]
-            print "</TD>"
-            print "<TD WIDTH=30%>"
+            __write__("<TR><TD WIDTH=30%>")
+            __write__(leftColumn[loopCounter])
+            __write__("</TD>")
+            __write__("<TD WIDTH=30%>")
             try:
-                print rightColumn[loopCounter]
-                print "</TD></TR>"
+                __write__(rightColumn[loopCounter])
+                __write__("</TD></TR>")
             except:
-                print " "
-                print "</TD></TR>"
+                __write__(" ")
+                __write__("</TD></TR>")
                 {{endscript}}
-        <BR><BR>
-        <HR>
-        <BR></P>
-        
-        <CENTER>
-        <TABLE>
-        <TR><TD>
-        
         </TD></TR>
         </TABLE>
         </CENTER>
-        
+
         <BR>
         <HR>
         
         </FONT>
         </BODY>
         </HTML>"""
-        
+
+        #Temp bit: checking what sourceString is set to at this point...
+        #print "sourceString = ", sourceString
+        #print;print;print
+
         mod = preppy.getModule("dummy_module", sourcetext=sourceString)
         modDataList=[]
         tempDict={'temp':'temporary variable'}
@@ -115,15 +118,46 @@ class SimpleTestCase(unittest.TestCase):
 
         #this way it goes to the list and also gets printed        
         mod.run(tempDict, __write__ = modDataList.append)
-        print 'length of list is %d' % len(modDataList)
+        #print 'length of list is %d' % len(modDataList)
         output=string.join (modDataList,'')
         outFile=open('sample007.html', 'w')
         outFile.write(output)
         outFile.close()
+        print 'wrote sample007.html'
+
+    def check08BigDictionary500(self):
+        # creates a 500 item dictionary of random numbers
+        howBig = 500
+        d = makeBigDictionary(howBig)
+        # print d
+        processTest('sample008', d)
+
+##    def check09BiggerDictionary1000(self):
+##        # creates a 1,000 item dictionary of random numbers
+##        howBig = 1000
+##        d = makeBigDictionary(howBig)
+##        # print d
+##        processTest('sample009', d)
+##
+##    def check10HugeDictionary10000(self):
+##        # creates a 10,000 item dictionary of random numbers
+##        howBig = 10000
+##        d = makeBigDictionary(howBig)
+##        # print d
+##        processTest('sample010', d)
+
+    def check11_StringKeysThatLookLikeIntegers(self):
+        # This test case doesn't work as expected
+        processTest('sample011', {'1':'crash','2':'burn'})
+
+    def check12_StringKeysThatLookLikeFloats(self):
+        # This test case doesn't work as expected either
+        processTest('sample012', {'1.01':'crash','2.78':'burn'})
             
 suite = unittest.makeSuite(SimpleTestCase,'check')
 
 def processTest(filename, dictionary={}):
+    #print 'processTest:',dictionary
     root, ext = os.path.splitext(filename)
     outFileName = root + '.html'
     mod = preppy.getModule(root)
@@ -142,6 +176,20 @@ def clean(dirname='.'):
         if os.path.isfile(root + '.pyc'):
             os.remove(root + '.pyc')
             
+def makeBigFile(howBig, dictionary={}):
+    # Just a placeholder at the moment...
+    pass
+
+def makeBigDictionary(howBig):
+    # This function creates a dictionary of howBig * random numbers  
+    dictionary = {}
+    for loopCounter in range(0,howBig):
+        randNumber = random.randint(1,1000)
+        key = 'key' + str(loopCounter)
+        dictionary[key]=randNumber
+    #    print loopCounter, randNumber
+    # print dictionary
+    return dictionary
         
 
 if __name__=='__main__':
