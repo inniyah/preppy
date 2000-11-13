c331c830ce21f3a4505d0cb07d59ec1081900612
john <john@reportlab.com>
2000-11-13 07:39:21 +0000

    Tidied up HTML (making tags UPPERCASE etc), and added a new test case.

--- test/check_basics.py
+++ test/check_basics.py
@@ -1,8 +1,6 @@
 #preppy test
-import os
+import os, glob, string
 from rlextra.preppy import preppy
-import glob
-
 from reportlab.test import unittest
 
 
@@ -20,11 +18,109 @@ class SimpleTestCase(unittest.TestCase):
     def check5Escapes(self):
         processTest('sample005')
     def check6NoPythonFile(self):
-        mod = preppy.getModule('sample006', verbose=1, savefile=0)
+        mod = preppy.getModule('sample006', savefile=0)
         outFile = open('sample006.html', 'w')
         mod.run(dictionary={}, outputfile = outFile)
         outFile.close()
+        print 'wrote sample006.html'
 
+    def check7NoOutputFile(self):
+        sourceString = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+        <HTML>
+        <HEAD>
+        <TITLE>ReportLab Preppy Test Suite 007</TITLE>
+        </HEAD>
+        
+        <BODY>
+        <FONT COLOR=#000000>
+        <TABLE BGCOLOR=#0000CC BORDER=0 CELLPADDING=0 CELLSPACING=0 WIDTH=100%>
+        <TR>
+        <TD>
+        <FONT COLOR=#FFFFFF>
+        <CENTER>
+        <H1>Preppy Test 007 - Creating A Module Without Using The Filesystem</H1>
+        </CENTER>
+        </FONT>
+        </TD>
+        </TR>
+        </TABLE>
+        
+        <BR>
+
+        This test creates a preppy source module in memory. It does <I>not</I> have a preppy source file (a file ending in '.prep')
+        
+        <H2>Expected Output</H2>
+        <P>
+        
+        Below is a listing of the files in the same directory as this HTML file. There should be no file called <I>sample007.prep</I>.
+        
+        <BR><BR><CENTER>
+        <TABLE>
+        {{script}}
+        import os
+        leftColumn = []
+        rightColumn = []
+        
+        fileList = os.listdir(os.getcwd())
+        
+        for item in range(0,len(fileList),2):
+            if os.path.isfile(fileList[item]):
+                leftColumn.append(fileList[item])
+        
+        for item in range(1,len(fileList)-1,2):
+            if os.path.isfile(fileList[item]):
+                rightColumn.append(fileList[item])
+        
+        for loopCounter in range(0, len(leftColumn)):
+            print "<TR><TD WIDTH=30%>"
+            print leftColumn[loopCounter]
+            print "</TD>"
+            print "<TD WIDTH=30%>"
+            try:
+                print rightColumn[loopCounter]
+                print "</TD></TR>"
+            except:
+                print " "
+                print "</TD></TR>"
+                {{endscript}}
+        <BR><BR>
+        <HR>
+        <BR></P>
+        
+        <CENTER>
+        <TABLE>
+        <TR><TD>
+        
+        </TD></TR>
+        </TABLE>
+        </CENTER>
+        
+        <BR>
+        <HR>
+        
+        </FONT>
+        </BODY>
+        </HTML>"""
+        
+        mod = preppy.getModule("dummy_module", sourcetext=sourceString)
+        modDataList=[]
+        tempDict={'temp':'temporary variable'}
+
+        # this way it goes to the string
+        #import cStringIO
+        #f = cStringIO.StringIO()
+        #mod.run(tempDict, outputfile=f)
+        #f.seek(0)
+        #output = f.read()
+
+        #this way it goes to the list and also gets printed        
+        mod.run(tempDict, __write__ = modDataList.append)
+        print 'length of list is %d' % len(modDataList)
+        output=string.join (modDataList,'')
+        outFile=open('sample007.html', 'w')
+        outFile.write(output)
+        outFile.close()
+            
 suite = unittest.makeSuite(SimpleTestCase,'check')
 
 def processTest(filename, dictionary={}):
@@ -55,24 +151,12 @@ if __name__=='__main__':
         if arg == 'clean':
             clean()
             sys.exit()
-        elif arg == 'old':
-            clean()
-            dictionary = {}
-            processTest('sample001')
-            processTest('sample002')
-            processTest('sample003')
-            dictionary = {"song1": {"songtitle": "The Lumberjack Song", "by": "Monthy Python"},
-                          "song2": {"songtitle": "My Way", "by": "Frank Sinatra"}}
-            processTest('sample004', dictionary)
-            processTest('sample005')
-#            processTest('sample006')
-            print 'please read all sample*.html files'
-            sys.exit()
+
         else:
             print 'argument not recognised!'
     else:
         runner = unittest.TextTestRunner()
         runner.run(suite)
-        print 'please read all sample*.html files'
+        print '\nplease read all sample*.html files'
         
     
