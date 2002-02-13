2bc8b869c18da41b846e0e0a165792ea661200fe
robin <robin@reportlab.com>
2002-02-13 17:42:07 +0000

    Ensure non-zero length

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#copyright ReportLab Inc. 2001-2002
+#see license.txt for license details
+#history www.reportlab.co.uk/rl-cgi/viewcvs.cgi/rlextra/rml2pdf/__init__.py
+#$Header: /rl_home/xxx/repository/rlextra/preppy/__init__.py,v 1.2 2002/02/13 17:41:23 robin Exp $
+
