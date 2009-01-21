74160877f44506106c051def689014e49396e354
rgbecker <rgbecker@localhost>
2009-01-21 12:00:42 +0000

    copy fidweb2 to isa2009

---
+++ __init__.py
@@ -0,0 +1,3 @@
+#copyright ReportLab Inc. 2001-2006
+#see license.txt for license details
+#history www.reportlab.co.uk/rl-cgi/viewcvs.cgi/rlextra/rml2pdf/__init__.py
---
+++ test/00readme.txt
@@ -0,0 +1,2 @@
+Execute check_basics.py.  This requires
+PyUnit's unittest.py which is in reportlab.test.
