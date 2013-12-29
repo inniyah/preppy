ef1c59f7b24591f1d3745e20e028602290b2a97c
robin <robin@reportlab.com>
2013-12-29 14:08:26 +0000

    test: more fixes

--- test/check_load.py
+++ test/check_load.py
@@ -8,7 +8,7 @@
 # $Date$
 
 
-import os, glob, string, random
+import os, glob, random
 import preppy
 import unittest
 
@@ -200,7 +200,7 @@ def makeBigDictionary(howBig):
     dictionary = {}
     for loopCounter in range(0,howBig):
         randNumber = random.randint(1,1000)
-        key = "key"+string.zfill(loopCounter, 4)
+        key = "key"+str.zfill(loopCounter, 4)
         dictionary[key]=randNumber
     return dictionary
 
