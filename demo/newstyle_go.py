8fa838ec422c37a90fdb36347e4bc0b91894aa8f
robin <robin@reportlab.com>
2021-06-25 08:52:57 +0100

    fix and extend newstyle demo

--- demo/newstyle_go.py
+++ demo/newstyle_go.py
@@ -1,12 +1,11 @@
-#Copyright ReportLab Europe Ltd. 2000-2015
+#Copyright ReportLab Europe Ltd. 2000-2021
 #see license.txt for license details
 
 ## new style calling
 def run():
     from preppy import getModule
     module = getModule("newstyle.prep", verbose=1)
-    print module.get('fred', 'm')
-
+    print(module.get([('fred', 'm'),('bill','f'),('bacterium','other'),('jill','m')]))
 
 if __name__=='__main__':
     run()
