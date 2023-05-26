3033a4554bfb97ea49753c3cee717e53add77b53
robin <robin@reportlab.com>
2023-05-26 15:14:52 +0100

    add modernised PreppyImporter for python 3.12; version --> 4.2.2

--- preppy.py
+++ preppy.py
@@ -33,7 +33,7 @@ since unix applications may run as a different user and not have the needed
 permission to store compiled modules.
 
 """
-VERSION = '4.2.1'
+VERSION = '4.2.2'
 __version__ = VERSION
 
 USAGE = """
@@ -74,6 +74,7 @@ isPy38 = isPy3 and sys.version_info.minor>=8
 isPy39 = isPy3 and sys.version_info.minor>=9
 isPy310 = isPy3 and sys.version_info.minor>=10
 isPy311 = isPy3 and sys.version_info.minor>=11
+isPy312 = isPy3 and sys.version_info.minor>=12
 _usePyCache = isPy3 and False                   #change if you don't have legacy ie python 2.7 usage
 from xml.sax.saxutils import escape as xmlEscape
 from collections import namedtuple
@@ -1329,41 +1330,76 @@ def installImporter():
     #add to them.
     global _preppy_importer
     if _preppy_importer is None:
-        class PreppyImporter(object):
-            "This allows prep files to be imported."
-            def __init__(self):
-                self.prepPath = None
-
-            def find_module(self, name, path=None):
-                if self.prepPath: return
-                if path:
-                    for p in path:
-                        prepPath = os.path.join(p,name.split('.')[-1] + '.prep')
-                        if os.path.isfile(prepPath): break
-                    else:
-                        return
-                else:
-                    prepPath = name + '.prep'
-                    if not os.path.isfile(prepPath): return
-
-                self.prepPath = prepPath
-                return self
-
-            def load_module(self,name):
-                if not self.prepPath: return
-                try:
-                    #compile WITHOUT IMPORTING to avoid triggering recursion
+        if isPy312:
+            import importlib.abc
+            import importlib.machinery
+            class _PreppyLoader(importlib.abc.Loader):
+                def create_module(self, spec):
+                    #print(f'+++++ create_module({spec!r})')
+                    name = spec.name
                     try:
-                        m = compileModule(self.prepPath, verbose=_verbose, importModule=0, existing_module=sys.modules.get(name,None))
+                        m = compileModule(spec.origin, verbose=_verbose, importModule=0, existing_module=sys.modules.get(name,None))
                     except:
                         traceback.print_exc()
                         raise
-                    m.__loader__ = self
+                    m.__loader__ = spec.loader
                     m.__name__ = name
                     sys.modules[name] = m
                     return m
-                finally:
+
+                def exec_module(self, module):
+                    pass
+
+            class PreppyImporter(importlib.abc.MetaPathFinder):
+                def find_spec(self, name, path, target=None):
+                    #print(f'+++++ find_spec({name!r},{path!r})')
+                    prepName = name.split('.')[-1] + '.prep'
+                    if not path:
+                        path = sys.path[:]
+                        if '' in path: path.remove('')
+                        path.insert(0,os.getcwd())
+                    for p in path:
+                        if p=='':
+                            p = os.getcwd()
+                        found = os.path.join(p,prepName)
+                        if os.path.isfile(found):
+                            return importlib.machinery.ModuleSpec(name, _PreppyLoader(), origin=found)
+        else:
+            class PreppyImporter(object):
+                "This allows prep files to be imported."
+                def __init__(self):
                     self.prepPath = None
+
+                def find_module(self, name, path=None):
+                    if self.prepPath: return
+                    if path:
+                        for p in path:
+                            prepPath = os.path.join(p,name.split('.')[-1] + '.prep')
+                            if os.path.isfile(prepPath): break
+                        else:
+                            return
+                    else:
+                        prepPath = name + '.prep'
+                        if not os.path.isfile(prepPath): return
+
+                    self.prepPath = prepPath
+                    return self
+
+                def load_module(self,name):
+                    if not self.prepPath: return
+                    try:
+                        #compile WITHOUT IMPORTING to avoid triggering recursion
+                        try:
+                            m = compileModule(self.prepPath, verbose=_verbose, importModule=0, existing_module=sys.modules.get(name,None))
+                        except:
+                            traceback.print_exc()
+                            raise
+                        m.__loader__ = self
+                        m.__name__ = name
+                        sys.modules[name] = m
+                        return m
+                    finally:
+                        self.prepPath = None
         _preppy_importer = PreppyImporter()
         sys.meta_path.insert(0,_preppy_importer)
 
